5b8a9a2836fa59effd299a8139993c15fc215cf4
Ngoc Huynh <ngochuynh1608@gmail.com>
2021-05-26 07:55:24 +0700

    update homepage

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -1,12 +1,68 @@
 <?php
 class ControllerCommonHome extends Controller {
 	public function index() {
+		$this->load->model('catalog/category');
+		$this->load->model('tool/image');
 		$this->document->setTitle($this->config->get('config_meta_title'));
 		$this->document->setDescription($this->config->get('config_meta_description'));
 		$this->document->setKeywords($this->config->get('config_meta_keyword'));
 
 		if (isset($this->request->get['route'])) {
 			$this->document->addLink($this->config->get('config_url'), 'canonical');
+		}
+
+
+		$data['categories'] = array();
+
+		$categories = $this->model_catalog_category->getCategories(0);
+
+		foreach ($categories as $category) {
+				$image="";
+				if ($category['image']) {
+					$image = $this->model_tool_image->resize($category['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_category_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_category_height'));
+				} else {
+					$image = $this->model_tool_image->resize('placeholder,png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_category_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_category_height'));
+				}
+				// Level 2
+				$children_data = array();
+
+				$children = $this->model_catalog_category->getCategories($category['category_id']);
+
+
+				foreach ($children as $child) {
+					$filter_data = array(
+						'filter_category_id'  => $child['category_id'],
+						'filter_sub_category' => true
+					);
+					$children2_data=array();
+                    $children2 = $this->model_catalog_category->getCategories($child['category_id']);
+
+                    foreach ($children2 as $child2) {
+    					$children2_data[] = array(
+                            'category_id' => $child2['category_id'],
+    						'name'  => $child2['name'],
+    						'href'  => $this->url->link('product/category', 'path=' . $child2['category_id']),
+    					);
+                        
+                    }
+
+					$children_data[] = array(
+						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'name'  => $child['name'],
+						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
+						'child' => $children2_data
+					);
+				}
+
+
+				// Level 1
+				$data['categories'][] = array(
+					'name'     => $category['name'],
+					'children' => $children_data,
+					'column'   => $category['column'] ? $category['column'] : 1,
+					'href'     => $this->url->link('product/category', 'path=' . $category['category_id']),
+					'image'		=> $image
+				);
 		}
 
 		$data['column_left'] = $this->load->controller('common/column_left');
--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -133,7 +133,7 @@ class ControllerProductCategory extends Controller {
 
 			$data['categories'] = array();
 
-			$results = $this->model_catalog_category->getCategories($category_id);
+			$results = $this->model_catalog_category->getCategories(0);
 
 			foreach ($results as $result) {
 				$filter_data = array(
@@ -143,7 +143,7 @@ class ControllerProductCategory extends Controller {
 
 				$data['categories'][] = array(
 					'name' => $result['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-					'href' => $this->url->link('product/category', 'path=' . $this->request->get['path'] . '_' . $result['category_id'] . $url),
+					'href' => $this->url->link('product/category', 'path='. $result['category_id'] . $url),
 					'thumb' => $this->model_tool_image->resize($result['image'] , $this->config->get('theme_' . $this->config->get('config_theme') . '_image_category_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_category_height'))
 				);
 			}
