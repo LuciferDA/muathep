9c1dd1a47c99d80e607e745679619549f6e1cd1c
Ngoc Huynh <ngochuynh1608@gmail.com>
2021-06-03 08:03:34 +0700

    fix home

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -47,7 +47,6 @@ class ControllerCommonHome extends Controller {
                     }
 
 					$children_data[] = array(
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
 						'name'  => $child['name'],
 						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
 						'child' => $children2_data
